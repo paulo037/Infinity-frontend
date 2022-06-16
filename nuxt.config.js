110fd5dded7aa2dbf221a2f7e8044f2ca4595810
Paulo <paulo.h.carneiro@ufv.br>
2022-06-15 23:57:33 -0300

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
     // Global page headers: https://go.nuxtjs.dev/config-head
     server: {     
-        port: process.env.PORT | 3000, // default: 3000     
+        // port: process.env.PORT | 3000, // default: 3000     
         // host: '0.0.0.0', 
         // default: localhost   
       },
