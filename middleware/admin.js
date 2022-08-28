b428d0f64171feeb545c8aa182abb47b05744e2c
Paulo <paulo.h.carneiro@ufv.br>
2022-08-28 11:29:40 -0300

    update middleware admin

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,11 +1,11 @@
 export default async ({ $auth, redirect, $axios }) => {
 
 
-    //const admin = await $axios.$get('/admin')
-      //  .catch(() => redirect("/"))
+    const admin = await $axios.$get('/admin')
+       .catch(() => redirect("/"))
 
 
-    if ($auth.$state.loggedIn && $auth.user.admin) {
+    if ($auth.$state.loggedIn && admin.admin) {
         return
     }
 
