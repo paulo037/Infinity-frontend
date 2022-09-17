84d50b747e96c2cd7db4e8618289aad66a7641b1
paulo071825 <paulo.h.carneiro@ufv.br>
2022-09-17 17:52:35 -0300

    configuracao axios signin

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,25 +1,14 @@
 
-export default function ({ $axios, res, $cookies, store }) {
-    $axios.onRequest(function (config) {
-        // Do something before request is sent
-        const token = $cookies.get('access_token')
-
-        $axios.defaults.headers['Authorization'] = `${token}`
-        console.log(token)
-        store.commit("toasted", { text: token })
-        return config;
-    }, function (error) {
-        // Do something with request error
-        return Promise.reject(error);
-    });
+export default  function ({ $axios, res, $cookies, store }) {
+    
 
     $axios.onResponse((config) => {
-        // if (config.config.url == "/signin") {
-        //     const token = config.data.access_token
-        //     config.data.access_token = true
-        //     $axios.defaults.headers['Authorization'] = `Bearer ${token}`
-        // }
-        // return config;
+        if (config.config.url == "/signin") {
+            const token = config.data.access_token
+            config.data.access_token = true
+            $axios.defaults.headers['Authorization'] = `Bearer ${token}`
+        }
+        return config;
 
     });
 }
