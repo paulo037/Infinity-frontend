0ed2080b20a914f6ca517dab539a6b332870d3aa
paulo071825 <paulo.h.carneiro@ufv.br>
2022-09-25 12:28:02 -0300

    added catch block on request

--- server-middleware/index.js
+++ server-middleware/index.js
@@ -8,7 +8,7 @@ import coockieParser from 'cookie-parser'
 
 
 app.use(coockieParser())
-app.use(cors({ credentials: true, origin: "*" }));
+app.use(cors({ credentials: true, origin: `http://localhost:3000` }));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -25,9 +25,10 @@ app.post('/tokens', async (req, res) => {
 app.post('/refreshTokens', async (req, res) => {
     console.log("refresh")
     const {refresh_token} = req.cookies
-    const response = await axios.post(`${process.env.URLBASE}/refreshToken`, {refresh_token:refresh_token}).catch(e => console.log(e))
-    console.log(response.data)
-    res.json(response.data).status(200)
+    await axios.post(`${process.env.URLBASE}/refreshToken`, {refresh_token:refresh_token})
+            .catch( (error) => res.status(400).send(error instanceof Error ? error.message : "Houve um erro inesperado"))
+            .then(response => res.json(response.data).status(200))
+    
 })
 
 
